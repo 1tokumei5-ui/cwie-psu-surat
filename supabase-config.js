d85b401dc2a4a5aae540587bb8c455ca9e985779
Library <1tokumei5@gmail.com>
2026-09-15 15:33:50 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://dthuzvbfyirkuhgarnvl.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_YkgWNkxOVXggx5WvyGLzXQ_2y0h-gf6';
+const SUPABASE_URL = 'https://eiagqhekxczwkhpfeesl.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_3sLobY_gHEJ0KCxXElSegQ_t29cXMnP';
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
