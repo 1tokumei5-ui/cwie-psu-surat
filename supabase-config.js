e72ccc8865a638cd2c2acbfaf73174faba3ae3d5
Library <1tokumei5@gmail.com>
2026-09-17 05:17:41 +0700

    Update Supabase URL and key in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://kfzqdhyrgopdzluutawu.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_P1Anip-6H10he0DoGmDUIw_e10Gatw6';
+const SUPABASE_URL = 'https://yxbjbmotphdfghljmxti.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_qe0hhJvNytTe_AiSrIyl2g_D6TUksYu';
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
