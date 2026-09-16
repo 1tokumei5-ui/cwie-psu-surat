ba0d75b3fa1e96f09196db7cfb95cf89b4972339
Library <1tokumei5@gmail.com>
2026-09-16 11:45:31 +0700

    Update Supabase URL and key in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://eiagqhekxczwkhpfeesl.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_3sLobY_gHEJ0KCxXElSegQ_t29cXMnP';
+const SUPABASE_URL = 'https://kfzqdhyrgopdzluutawu.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_P1Anip-6H10he0DoGmDUIw_e10Gatw6';
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
